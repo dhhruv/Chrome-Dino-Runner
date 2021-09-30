1b18c85372cefd10a82753e318f235ef1458db40
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-09-30 18:59:26 +0530

    Added binaryfiles.txt

--- chromedino.py
+++ chromedino.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame
 import os
-import threading
 import random
+import threading
+
+import pygame
+
 pygame.init()
 
 # Global Constants
@@ -12,38 +14,40 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption('Chrome Dino Runner')
+pygame.display.set_caption("Chrome Dino Runner")
 
-Ico = pygame.image.load('assets/DinoWallpaper.png')
+Ico = pygame.image.load("assets/DinoWallpaper.png")
 pygame.display.set_icon(Ico)
 
-RUNNING = [pygame.image.load(os.path.join('assets/Dino', 'DinoRun1.png'
-           )), pygame.image.load(os.path.join('assets/Dino',
-           'DinoRun2.png'))]
-JUMPING = pygame.image.load(os.path.join('assets/Dino', 'DinoJump.png'))
-DUCKING = [pygame.image.load(os.path.join('assets/Dino', 'DinoDuck1.png'
-           )), pygame.image.load(os.path.join('assets/Dino',
-           'DinoDuck2.png'))]
+RUNNING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun2.png")),
+]
+JUMPING = pygame.image.load(os.path.join("assets/Dino", "DinoJump.png"))
+DUCKING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck2.png")),
+]
 
-SMALL_CACTUS = [pygame.image.load(os.path.join('assets/Cactus',
-                'SmallCactus1.png')),
-                pygame.image.load(os.path.join('assets/Cactus',
-                'SmallCactus2.png')),
-                pygame.image.load(os.path.join('assets/Cactus',
-                'SmallCactus3.png'))]
-LARGE_CACTUS = [pygame.image.load(os.path.join('assets/Cactus',
-                'LargeCactus1.png')),
-                pygame.image.load(os.path.join('assets/Cactus',
-                'LargeCactus2.png')),
-                pygame.image.load(os.path.join('assets/Cactus',
-                'LargeCactus3.png'))]
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus3.png")),
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus3.png")),
+]
 
-BIRD = [pygame.image.load(os.path.join('assets/Bird', 'Bird1.png')),
-        pygame.image.load(os.path.join('assets/Bird', 'Bird2.png'))]
+BIRD = [
+    pygame.image.load(os.path.join("assets/Bird", "Bird1.png")),
+    pygame.image.load(os.path.join("assets/Bird", "Bird2.png")),
+]
 
-CLOUD = pygame.image.load(os.path.join('assets/Other', 'Cloud.png'))
+CLOUD = pygame.image.load(os.path.join("assets/Other", "Cloud.png"))
 
-BG = pygame.image.load(os.path.join('assets/Other', 'Track.png'))
+BG = pygame.image.load(os.path.join("assets/Other", "Track.png"))
 
 
 class Dinosaur:
@@ -121,7 +125,6 @@ class Dinosaur:
 
 
 class Cloud:
-
     def __init__(self):
         self.x = SCREEN_WIDTH + random.randint(800, 1000)
         self.y = random.randint(50, 100)
@@ -139,7 +142,6 @@ class Cloud:
 
 
 class Obstacle:
-
     def __init__(self, image, type):
         self.image = image
         self.type = type
@@ -156,7 +158,6 @@ class Obstacle:
 
 
 class SmallCactus(Obstacle):
-
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
@@ -164,7 +165,6 @@ class SmallCactus(Obstacle):
 
 
 class LargeCactus(Obstacle):
-
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
@@ -172,7 +172,6 @@ class LargeCactus(Obstacle):
 
 
 class Bird(Obstacle):
-
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
@@ -196,7 +195,7 @@ def main():
     x_pos_bg = 0
     y_pos_bg = 380
     points = 0
-    font = pygame.font.Font('freesansbold.ttf', 20)
+    font = pygame.font.Font("freesansbold.ttf", 20)
     obstacles = []
     death_count = 0
 
@@ -206,7 +205,7 @@ def main():
         if points % 100 == 0:
             game_speed += 1
 
-        text = font.render('Points: ' + str(points), True, (0, 0, 0))
+        text = font.render("Points: " + str(points), True, (0, 0, 0))
         textRect = text.get_rect()
         textRect.center = (1000, 40)
         SCREEN.blit(text, textRect)
@@ -264,25 +263,20 @@ def menu(death_count):
     run = True
     while run:
         SCREEN.fill((255, 255, 255))
-        font = pygame.font.Font('freesansbold.ttf', 30)
+        font = pygame.font.Font("freesansbold.ttf", 30)
 
         if death_count == 0:
-            text = font.render('Press any Key to Start', True, (0, 0,
-                               0))
+            text = font.render("Press any Key to Start", True, (0, 0, 0))
         elif death_count > 0:
-            text = font.render('Press any Key to Restart', True, (0, 0,
-                               0))
-            score = font.render('Your Score: ' + str(points), True, (0,
-                                0, 0))
+            text = font.render("Press any Key to Restart", True, (0, 0, 0))
+            score = font.render("Your Score: " + str(points), True, (0, 0, 0))
             scoreRect = score.get_rect()
-            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
-                                + 50)
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
             SCREEN.blit(score, scoreRect)
         textRect = text.get_rect()
         textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         SCREEN.blit(text, textRect)
-        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT
-                    // 2 - 140))
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
